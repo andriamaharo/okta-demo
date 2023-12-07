fa6bee1dadf118ed31c4a9f0c697b4e87e2e1634
Ricky <ricky.andriamaharo@proximity.fr>
2023-12-07 15:56:40 +0100

    commit

---
+++ tests/environment.js
@@ -0,0 +1,5 @@
+process.env.AUTH0_SECRET = 'eef07e350aa7183f1bd31230dd012a4c5dddbc6b5a5d72ff783694e07204a39a';
+process.env.AUTH0_ISSUER_BASE_URL = 'dev-secure.pampers.com.br';
+process.env.AUTH0_BASE_URL = 'http://localhost:3000';
+process.env.AUTH0_CLIENT_ID = '5VEGgEc3FqVcgDrDYzrioF24xv1duCPB';
+process.env.AUTH0_CLIENT_SECRET = '5J0nIYHUnbF7RfPSo0hME0nGP-qvRRK4PZtoZ3VsKKOktAQanKU24x6_F_cAmojL';
